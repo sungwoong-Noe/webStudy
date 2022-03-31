da462ee82c49443b739436bc8d6903af4d515228
noseong-ung <asdz453@gmail.com>
2022-03-31 20:20:49 +0900

    Initial commit

---
+++ src/main/java/com/example/webstudy/WebStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.webstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStudyApplication.class, args);
+	}
+
+}
